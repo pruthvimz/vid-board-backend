42fe20f557e3815974927dfd79f539d99e7edfba
pruthvimz <pruthvi.zandawala@gmail.com>
2018-07-08 04:17:52 +0530

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const SRC_DIR = path.resolve(__dirname, "");
